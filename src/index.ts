b5e747be79c37c3917ded63c21d4a21294ec7508
Gitea <gitea@fake.local>
2020-12-18 16:13:45 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ declare const module : WebpackHotModule;
                 title: "Team BS Car",
                 version: "1.0.3",
                 description:
-                    "To launch request with this documentation you need to send a request to /users/login at the end with the credentials admin/admin and then put the token in authorize in the top right corner. After that you will be able to test all of our request within the documentation. To launch request within your app you need to do the same. Note: the 500 internal server error when launching without token is due to heroku."
+                    "To launch request with this documentation you need to send a request to /users/login at the end with the credentials admin/admin and then put the token in authorize in the top right corner. After that you will be able to test all of our request within the documentation. To launch request within your app you need to do the same.Note: the 500 internal server error when launching without token is due to heroku."
             },
             components: {
                 securitySchemes: {
